8d50913b984ba12a3360af9879ed4427f3460ac7
Justin <justin.tseng@kingwaytek.com>
2022-05-05 17:50:24 +0800

    17 transitioning to the add new question view controller

---
+++ ExamApp/Controllers/AddQuestionTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddQuestionTableViewController.swift
+//  ExamApp
+//
+//  Created by justin on 2022/5/5.
+//  Copyright © 2022 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddQuestionTableViewController: UITableViewController {
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    @IBOutlet weak var pointTextField: UITextField!
+    @IBOutlet weak var isCorrectSegmetedControl: UISegmentedControl!
+    
+    @IBAction func close() {
+        
+    }
+    
+    @IBAction func save() {
+        
+    }
+}
